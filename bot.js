7a307e8633049a5279b698fa47e9292326298bfd
DMforall <abramring@gmail.com>
2020-03-17 07:53:16 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ function commandHandler(relThis, command){
    //command.text = command.text.replace( temp.substr( command.text.search(/\+/) ) , ""); 
   } 
 
-  if ( command.text.includes("d") ) { //Check for input xdy
+  if ( command.text.search(/\d/) > -1  ) { //Check for input xdy
    //dice setup
     
    //rollCount = parseInt( command.text.slice(0, command.text.indexOf("d")  )  );
